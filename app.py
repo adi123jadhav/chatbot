53aa6fc712495a932002d3fb69b39cd62dc7d21e
adi123jadhav <149108233+adi123jadhav@users.noreply.github.com>
2025-01-19 15:54:05 +0530

    anuska

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ from dotenv import load_dotenv
 import os
 import json
 import requests
-
+from  langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters.character import CharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
 from langchain.memory import ConversationBufferMemory
@@ -18,22 +19,32 @@ from langchain_core.documents import Document
 
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 LANGCHAIN_API_KEY = "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"
-GROQ_API_KEY = "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"
+GROQ_API_KEY = "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"
 PINECONE_API_KEY = "pcsk_7dC9j_PafKN332jv8kQ88VxCgwpVQyGYZHWFerKa34HZA42TA9jn9ezgQTHhZVZf23yDb"
 app = Flask(__name__)
 
 
 # Load initial documents
 
-pc =Pinecone("pcsk_6ui2vW_6jcX78323C6hpsnZLwbRLXuBFPFu4dAQhmjJN4DN2QFantkQcJp456UDT8ss6jd")
+pc = Pinecone(PINECONE_API_KEY)
 index = pc.Index("consumercare")
 # Initialize embeddings and vector store
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
 
-vector_store=PineconeVectorStore(embedding=embeddings,index=index)
+
+def is_data_already_indexed(index, expected_count):
+    """Check if the dataset is already indexed."""
+    stats = index.describe_index_stats()
+    return stats.get("total_vector_count", 0) >= expected_count
 
 
+loader = CSVLoader("Bitext_Sample_Customer_Support_Training_Dataset_27K_responses-v11.csv",encoding="utf-8")
+document = loader.load()
 
+vector_store = PineconeVectorStore(embedding=embeddings, index=index)
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=0)
+doc_chunks = text_splitter.split_documents(document)
 
 # Initialize LLM and memory
 llm = ChatGroq(
@@ -42,7 +53,10 @@ llm = ChatGroq(
     temperature=0.5
 )
 retriever = vector_store.as_retriever()
-memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+memory = ConversationBufferMemory(
+    memory_key="chat_history",
+    return_messages=True
+)
 chain = ConversationalRetrievalChain.from_llm(
     llm=llm,
     retriever=retriever,
@@ -50,6 +64,14 @@ chain = ConversationalRetrievalChain.from_llm(
     memory=memory,
     verbose=True
 )
+
+if not is_data_already_indexed(index, len(doc_chunks)):
+    uuids = [str(uuid4()) for _ in range(len(doc_chunks))]
+    vector_store = PineconeVectorStore(embedding=embeddings, index=index)
+    vector_store.add_documents(documents=doc_chunks, ids=uuids)
+else:
+    print("Data is already indexed. Skipping re-indexing.")
+
 
 
 def format_response(response):
@@ -90,5 +112,5 @@ def chat():
 
 
 
-if __name__ == '_main_':
+if __name__ == '__main__':
     app.run(debug=True)
